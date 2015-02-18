12d86df296e0a41eebe110afa82389d4837d1c19
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-02-18 15:57:51 +0100

    hello deposit && helo withdraw

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', appRoutes);
 app.use('/admin', adminRoutes);
